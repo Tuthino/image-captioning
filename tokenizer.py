7065746f3c7645f00654304fc4453bca3b0f70f2
nico.enghardt <nico.enghardt@hotmail.com>
2024-12-05 18:06:51 +0100

    self-written (corrupted) tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,16 +1,13 @@
 import torch
 from transformers import LlamaTokenizerFast, LlamaTokenizer
 from dataset import *
-#tokenizer = torch.hub.load('huggingface/pytorch-transformers', 'tokenizer', 'bert-base-uncased')
+from torch import nn
 tokenizer = LlamaTokenizerFast.from_pretrained("hf-internal-testing/llama-tokenizer")
-#import pdb; pdb.set_trace()
 tokenizer.add_special_tokens({"pad_token":"<pad>"})
 
 num_tokens = tokenizer.vocab_size + 1
 
-#from tokenizer import *
-
-dataset = create_simple_dataset(csv_path, image_folder, size=(224, 224), max=50)
+dataset = create_simple_dataset(csv_path, image_folder, size=(224, 224), max=None)
 
 train_set, val_set, test_set = split_dataset(dataset)
 
@@ -26,47 +23,74 @@ def update_set(this_set, text):
     return this_set
 
 tokens_train = set({})
-for text, _ in train_set:
-    update_set(tokens_train, text)
+for text, _ in train_set: update_set(tokens_train, text)
 print(tokens_train)
 
-tokenizer = LlamaTokenizer(tokenizer_object=None)
-tokenizer.add_tokens(tokens_train)
-vocab = {token: idx for idx, token in enumerate(tokens_train)}
-tokenizer.vocab = vocab
+token2idx = {token: idx for idx, token in enumerate(tokens_train)}
+token_tree = {i: {} for i in range(10,-1, -1)}
 
-pass
-# characters = ['<SOS>', '<EOS>', '<PAD>', '     ', '!', '"', '#', '&', "'", '(', ')', ',', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '=', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-# num_tokens = len(characters)
-# idx2char = {k: v for k, v in enumerate(characters)}
-# char2idx = {v: k for k, v in enumerate(characters)}
-
-# max_text_length = 200
-
-# def indices2text(indices):
-    
-#     text = ""
-    
-#     end_index = char2idx["<EOS>"]
-#     indices = torch.argmax(indices, dim=1)
-    
-#     for idx in indices:
+for token, idx in token2idx.items():
         
-#         if idx is end_index: break
-        
-#         text += idx2char[int(idx)]
-        
-#     return text
+    try:
 
-# def text2indices(text):
-    
-#     indices = []
-    
-#     for char in list(text):
+        token_tree[len(token)][token] = idx
         
-#         if char not in char2idx: break
-        
-#         indices.append(char2idx[char])
+    except:
+        print(len(token))
 
-#        return torch.stack(nn.functional.one_hot(indices))
+
+num_tokens = len(token2idx)
+idx2token = {k: v for k, v in enumerate(token2idx)}
+
+max_text_length = 200
+
+def indices2text(indices):
+    
+    text = ""
+    
+    end_index = -1 #token2idx["<EOS>"]
+    #indices = torch.argmax(indices, dim=1)
+    
+    for idx in indices:
+        
+        if idx is end_index: break
+        
+        text += idx2token[int(idx)]
+        
+    return text
+
+def text2indices(text):
+    
+    indices = []
+    
+    text = text.replace(" ", "")
+    
+    while len(text) > 0:
+        
+        found = False
+    
+        for length, sub_tree in token_tree.items():
+            
+            sub_part = text[:length]
+            
+            if sub_part in sub_tree:
+                
+                indices.append(sub_tree[sub_part])
+                print(length, indices[-1], text, sub_part)
+            
+                text = text[length:]
+                found = True
+  
+                break
+            
+        if not found:
+            text = text[1:]
+            indices.append(-1)
+        
+
+    return torch.stack(nn.functional.one_hot(torch.Tensor(indices).type(torch.LongTensor)))
+   
+if __name__ == '__main__':
+    
+    print(indices2text(torch.Tensor([2,4,3,5,6,])))
+    print(text2indices("Paneer With Burst Cherry"))
