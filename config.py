addbad125f369945bb0137dee55ceba5fafb82da
Tuthino <reyio@vivaldi.net>
2024-12-08 14:23:55 +0100

    add model saving model path to config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ csv_path = Path("Data/Food Ingredients and Recipe Dataset with Image Name Mappin
 image_folder = Path("Data/Food Images/Food Images/")
 
 max_tokens = 30
+resnet_model_save_path = Path("models/resnet_gru.pth")
